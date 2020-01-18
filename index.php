fee650596595edcac3da2e5cac91809a72f2dd2e
unknown <josuedaniel831@yahoo.com>
2020-01-17 19:38:42 -0700

    added dynamic post details such as author, date, and category through php

--- index.php
+++ index.php
@@ -17,9 +17,9 @@
   		while(have_posts()){
   			the_post(); ?>
   			<div class="post-item">
-  				<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></h2>
+  				<h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></h2>
   				<div class="metabox">
-  					<p>Posted by Josue on 1.17.20 in News</p>
+  					<p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(', '); ?></p>
 
 
   				</div>
